b4d08a90203e4b4bf0286a8d82e9c64cd6e4f0bb
Saurlax <saurlax@qq.com>
2026-03-13 23:39:39 +0800

    feat: init i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: [
-    '@nuxt/ui',
-    '@nuxtjs/i18n',
-    '@nuxtjs/mdc',
-    'nuxt-auth-utils',
-    '@nuxthub/core'
-  ]
-})
+    "@nuxt/ui",
+    "@nuxtjs/i18n",
+    "@nuxtjs/mdc",
+    "nuxt-auth-utils",
+    "@nuxthub/core",
+  ],
+  ui: {
+    fonts: false,
+  },
+  i18n: {
+    locales: ["zh"],
+  },
+});
