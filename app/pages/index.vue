3dddf2e425b89c6a44db55dee8d9bd0d852359aa
Saurlax <saurlax@qq.com>
2026-03-16 11:16:44 +0800

    feat: update home page with PageColumns

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -12,40 +12,36 @@ const vulnerabilityColumns = [
 </script>
 
 <template>
-  <UPageCard
-    class="my-8"
-    title="最新公告"
-    description="展示最近发布的公告。"
-    variant="naked"
-  >
-    <template #actions>
-      <ULink to="/" variant="text">查看更多</ULink>
-    </template>
+  <UPage>
+    <UPageBody>
+      <UPageColumns class="lg:columns-2">
+        <UPageCard title="最新公告" variant="naked">
+          <template #actions>
+            <ULink to="/" variant="text">查看更多</ULink>
+          </template>
 
-    <template #default>
-      <UTable
-        :data="announcements"
-        :columns="announcementColumns"
-        empty="暂无公告"
-      />
-    </template>
-  </UPageCard>
+          <template #default>
+            <UTable
+              :data="announcements"
+              :columns="announcementColumns"
+              empty="暂无公告"
+            />
+          </template>
+        </UPageCard>
+        <UPageCard title="最新漏洞" variant="naked">
+          <template #actions>
+            <ULink to="/vulnerabilities" variant="text">查看更多</ULink>
+          </template>
 
-  <UPageCard
-    title="最新漏洞"
-    description="展示最新创建的漏洞列表。"
-    variant="naked"
-  >
-    <template #actions>
-      <ULink to="/vulnerabilities" variant="text">查看更多</ULink>
-    </template>
-
-    <template #default>
-      <UTable
-        :data="vulnerabilities"
-        :columns="vulnerabilityColumns"
-        empty="暂无漏洞"
-      />
-    </template>
-  </UPageCard>
+          <template #default>
+            <UTable
+              :data="vulnerabilities"
+              :columns="vulnerabilityColumns"
+              empty="暂无漏洞"
+            />
+          </template>
+        </UPageCard>
+      </UPageColumns>
+    </UPageBody>
+  </UPage>
 </template>
